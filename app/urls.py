4b4cc51180745f1a24dc820b83b7118d4c435cbd
Francis Karagu <fkaragu@gmail.com>
2019-03-15 12:12:38 +0300

    Added welcome function

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,3 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.welcome)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
