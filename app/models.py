d55ef3c6130ad5bb26f89ebbe47b81106a0814e7
Francis Karagu <fkaragu@gmail.com>
2019-03-16 22:21:22 +0300

    Added project model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from pyuploadcare.dj.models import ImageField
 
-# Create your models here.
+class Project(models.Model):
+    photo = models.ImageField(upload_to='articles/', blank=True)
+    photo_caption = models.CharField(max_length = 50)
+    photo_link = models.CharField(max_length = 20)
+    post_date = models.DateTimeField(auto_now=True)
