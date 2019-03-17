c6a401d9062eb3508ef2710b4bb896742da18721
Francis Karagu <fkaragu@gmail.com>
2019-03-17 23:52:23 +0300

    Modified models

--- app/models.py
+++ app/models.py
@@ -5,11 +5,11 @@ from pyuploadcare.dj.models import ImageField
 
 class Project(models.Model):
     photo = models.ImageField(upload_to='articles/', blank=True)
-    photo_caption = models.CharField(max_length = 50)
-    photo_link = models.CharField(max_length = 20)
+    proj_name = models.CharField(max_length = 50)
+    proj_link = models.CharField(max_length = 20)
     post_date = models.DateTimeField(auto_now=True)
 
     @classmethod
     def search_project(cls, name):
-        pro = Project.objects.filter(photo_caption__icontains = name)
+        pro = Project.objects.filter(proj_name__icontains = name)
         return pro
--- award/urls.py
+++ award/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url,include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'', include('app.urls')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout, {"next_page": '/'})
 
 ]
