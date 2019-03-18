4339dc3251c94713042882acb6f02724b29468e9
Francis Karagu <fkaragu@gmail.com>
2019-03-18 10:38:01 +0300

    Added rating css

--- app/models.py
+++ app/models.py
@@ -5,11 +5,12 @@ from pyuploadcare.dj.models import ImageField
 
 class Project(models.Model):
     photo = models.ImageField(upload_to='articles/', blank=True)
-    proj_name = models.CharField(max_length = 50)
-    proj_link = models.CharField(max_length = 20)
+    proj_title = models.CharField(max_length = 25)
+    proj_link = models.CharField(max_length = 50)
+    proj_description = models.CharField(max_length = 2500)
     post_date = models.DateTimeField(auto_now=True)
 
     @classmethod
     def search_project(cls, name):
-        pro = Project.objects.filter(proj_name__icontains = name)
+        pro = Project.objects.filter(proj_title__icontains = name)
         return pro
